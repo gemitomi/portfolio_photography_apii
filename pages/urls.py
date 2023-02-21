5d00db21ef453a8687ea01d05579dc75809c28d4
Tom Gemi <gemitomi@hotmail.com>
2023-02-21 15:32:51 +0100

    update

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from .views import HomeView, SiteInfoView
 
 
 
 urlpatterns = [
-    
+    path("", HomeView.as_view()),
+    path("siteinfo/", SiteInfoView.as_view()),
 ]
 # path("", home_view),
   #  path("about/", about_view),
--- portfolio_photography/urls.py
+++ portfolio_photography/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("pages.urls"))
+    # path('api-auth/', include('rest_framework.urls'))  // csak akkor kell ha usereket akarunk használni
 ]
 
 if settings.DEBUG:
