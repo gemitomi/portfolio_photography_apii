d18e1d162216fecd5b7b4e6a8d590add7de26afe
Tom Gemi <gemitomi@hotmail.com>
2023-02-26 22:05:33 +0100

    update

--- pages/admin.py
+++ pages/admin.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 
-from .models import SiteInfo, About, Categories, Photo
+from .models import SiteInfo, About, Categories, Photo, LandingPage
 
 admin.site.register(SiteInfo)
 admin.site.register(About)
 admin.site.register(Categories)
+admin.site.register(LandingPage)
 
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ["image", "category"]
     search_fields = ["image"]
     list_filter = ["category"]
-
-
 
 admin.site.register(Photo, PhotoAdmin)
 
--- pages/urls.py
+++ pages/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 # from .views import HomeView, SiteInfoView
-from .views import SiteInfoView, AboutView, CategoryView, GalleryView
+from .views import SiteInfoView, AboutView, CategoryView, GalleryView, LandingPageView
 
 
 urlpatterns = [
     path("siteinfo/", SiteInfoView.as_view()),
     path("about/", AboutView.as_view()),
     path("categories/", CategoryView.as_view()),
-    path("gallery/", GalleryView.as_view())
+    path("gallery/", GalleryView.as_view()),
+    path("landing-page/", LandingPageView.as_view())   
 ]
 # path("", home_view),
   #  path("about/", about_view),
